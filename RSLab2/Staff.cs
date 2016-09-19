c32811bc3762bc9a89c7eeec41dc705f38f9b874
Ilya Krotkikh <ivashca99@gmail.com>
2016-09-20 02:47:24 +0300

    Added model Staff, amall changes from MainPage.

---
+++ RSLab2/Staff.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSLab2
+{
+    class Staff
+    {
+        private string _FirstName, _LastName, _Patronymic;
+
+        public string FirstName
+        {
+            get
+            {
+                return this._FirstName;
+            }
+                set
+            {
+                this._FirstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return this._LastName;
+            }
+            set
+            {
+                this._LastName = value;
+            }
+        }
+        public string Patronymic
+        {
+            get
+            {
+                return this._Patronymic;
+            }
+            set
+            {
+                this._Patronymic = value;
+            }
+        }
+
+        public string FIO {
+            get
+            {
+                return LastName + " " + FirstName + " " + Patronymic;
+            }
+        }
+
+        public Staff()
+        {
+            FirstName = LastName = Patronymic = "";   
+        }
+    }
+}
